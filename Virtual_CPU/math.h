f775aed587048ae48a70bd8d886be7e394ee6ff4
acool8868 <acool8868@gmail.com>
2024-12-15 22:07:39 +0530

    test

--- Virtual_CPU/math.h
+++ Virtual_CPU/math.h
@@ -1,1 +1,14 @@
 #pragma once
+
+void HalfAdder(bool a, bool b, bool &sum, bool &carry) {
+    sum = XOR(a, b);
+    carry = AND(a, b);
+}
+
+void FullAdder(bool a, bool b, bool cin, bool &sum, bool &carry) {
+    bool sum1, carry1, carry2;
+    HalfAdder(a, b, sum1, carry1);
+    HalfAdder(sum1, cin, sum, carry2);
+    carry = OR(carry1, carry2);
+}
+
