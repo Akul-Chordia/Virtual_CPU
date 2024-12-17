e4ca73b22b3395b34b31116258a88eb478e0926c
acool8868 <acool8868@gmail.com>
2024-12-17 21:56:32 +0530

    Rename variables, added 4-bit adder

--- Virtual_CPU/math.h
+++ Virtual_CPU/math.h
@@ -1,15 +1,23 @@
 #pragma once
 
 //Basic math blocks
-void HalfAdder(bool a, bool b, bool &sum, bool &carry) {
+void half_adder(bool a, bool b, bool &sum, bool &carry) {
     sum = XOR(a, b);
     carry = AND(a, b);
 }
 
-void FullAdder(bool a, bool b, bool cin, bool &sum, bool &carry) {
+void full_adder(bool a, bool b, bool cin, bool &sum, bool &carry) {
     bool sum1, carry1, carry2;
-    HalfAdder(a, b, sum1, carry1);
-    HalfAdder(sum1, cin, sum, carry2);
+    half_adder(a, b, sum1, carry1);
+    half_adder(sum1, cin, sum, carry2);
     carry = OR(carry1, carry2);
 }
+
+void four_bit_adder(bool a[4], bool b[4], bool cin, bool value[4], bool &carry){
+    full_adder(a[0], b[0], cin, value[0], carry);
+    full_adder(a[1], b[1], carry, value[1], carry);
+    full_adder(a[2], b[2], carry, value[2], carry);
+    full_adder(a[3], b[3], carry, value[3], carry);
+}
+
 
